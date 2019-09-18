897c1e8c60b9ae668f6afe5c469115ad72730e7e
Hatem Alimam <me@hatemalimam.com>
2019-09-18 12:03:14 +0300

    initial support for redux

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,112 +1,169 @@
-import invariant from 'invariant'
-import { startBlueprint, stopBlueprint, resetIdleStatusBlueprint, activityBlueprint, activityDetectionBlueprint } from './blueprints'
-import { IS_DEV, IDLESTATUS_ACTIVE, USER_ACTIVE, NEXT_IDLE_STATUS, RESET_IDLE_STATUS } from './constants'
-import localsync from 'localsync'
+import invariant from "invariant";
+import {
+  startBlueprint,
+  stopBlueprint,
+  resetIdleStatusBlueprint,
+  activityBlueprint,
+  activityDetectionBlueprint
+} from "./blueprints";
+import {
+  IS_DEV,
+  IDLESTATUS_ACTIVE,
+  USER_ACTIVE,
+  NEXT_IDLE_STATUS,
+  RESET_IDLE_STATUS,
+  ROOT_STATE_KEY
+} from "./constants";
+import localsync from "localsync";
 
-const STOP_TYPES = [ 'pointermove', 'MSPointerMove' ]
-const FILTER_TYPES = [ 'mousemove' ]
+const STOP_TYPES = ["pointermove", "MSPointerMove"];
+const FILTER_TYPES = ["mousemove"];
 
-const isBrowser = () => typeof window === 'object'
+const isBrowser = () => typeof window === "object";
 
 /** Detects whether the activity should trigger a redux update */
-const createShouldActivityUpdate = ({ log, thresholds }) => store => ({ type, pageX, pageY }) => {
-
-  if(STOP_TYPES.indexOf(type) !== -1)
-    return false
-  if(!FILTER_TYPES.indexOf(type) !== -1)
-    return true
+const createShouldActivityUpdate = ({ log, thresholds }) => store => ({
+  type,
+  pageX,
+  pageY
+}) => {
+  if (STOP_TYPES.indexOf(type) !== -1) return false;
+  if (!FILTER_TYPES.indexOf(type) !== -1) return true;
   /** If last event was not the same event type, trigger an update. */
-  const { lastActive, lastEvent } = selectIdleState(store.getState())
-  if(lastEvent.type !== type)
-    return true
+  const lastActive = store.getState().getIn([ROOT_STATE_KEY, "lastActive"]);
+  const lastEvent = store.getState().getIn([ROOT_STATE_KEY, "lastEvent"]);
+  if (lastEvent.type !== type) return true;
 
   /** If last mouse events coordinates were not within mouse threshold, trigger an update. */
-  const { x, y } = lastEvent
-  if((pageX && pageY && x && y) && Math.abs(pageX - x) < thresholds.mouse && Math.abs(pageY - y) < thresholds.mouse)
-    return false
-  return true
-}
-
-const selectIdleState = state => {
-  const { idle } = state
-  if(IS_DEV) {
-    invariant(idle, 'idle monitor state should have idle value')
-    invariant(typeof idle === 'object', 'idle monitor state should have type object')
-  }
-  return idle
-}
+  const { x, y } = lastEvent;
+  if (
+    pageX &&
+    pageY &&
+    x &&
+    y &&
+    Math.abs(pageX - x) < thresholds.mouse &&
+    Math.abs(pageY - y) < thresholds.mouse
+  )
+    return false;
+  return true;
+};
 
 const isRunning = (dispatch, getState) => {
-  const { isDetectionRunning } = selectIdleState(getState())
-  if(IS_DEV) {
-    invariant(isDetectionRunning, 'idle monitor state should have isDetectionRunning defined')
-    invariant(typeof isDetectionRunning === 'boolean', 'isDetectionRunning should be type boolean')
+  const isDetectionRunning = getState().getIn([
+    ROOT_STATE_KEY,
+    "isDetectionRunning"
+  ]);
+  if (IS_DEV) {
+    invariant(
+      isDetectionRunning,
+      "idle monitor state should have isDetectionRunning defined"
+    );
+    invariant(
+      typeof isDetectionRunning === "boolean",
+      "isDetectionRunning should be type boolean"
+    );
   }
-  return isDetectionRunning
-}
-
+  return isDetectionRunning;
+};
 
 const createLocalSync = ({ log, activity, getIsTransition }) => store => {
   const action = isActive => {
-    if(isActive)
-      return { isActive, lastActive: Date.now() }
-    else
-      return { isActive }
-  }
+    if (isActive) return { isActive, lastActive: Date.now() };
+    else return { isActive };
+  };
 
   const handler = (value, old, url) => {
-    log.info({ value, old, url }, 'local sync')
-    if(value.isActive)
-      store.dispatch(activity({ type: 'local', isTransition: getIsTransition() }))
-  }
-  return localsync('idlemonitor', action, handler)
-}
+    log.info({ value, old, url }, "local sync");
+    if (value.isActive)
+      store.dispatch(
+        activity({ type: "local", isTransition: getIsTransition() })
+      );
+  };
+  return localsync("idlemonitor", action, handler);
+};
 
-
-const createActivityDetection = ({ log, thresholds, activeEvents, activity, activityDetection, getIsTransition }) => store => {
-  const { dispatch } = store
-  const shouldActivityUpdate = createShouldActivityUpdate({ log, thresholds })(store)
+const createActivityDetection = ({
+  log,
+  thresholds,
+  activeEvents,
+  activity,
+  activityDetection,
+  getIsTransition
+}) => store => {
+  const { dispatch } = store;
+  const shouldActivityUpdate = createShouldActivityUpdate({ log, thresholds })(
+    store
+  );
   /** One of the event listeners triggered an activity occurrence event. This gets spammed */
   const onActivity = e => {
-    if (!shouldActivityUpdate(e))
-      return
-    dispatch(activity({ x: e.pageX, y: e.pageY, type: e.type, isTransition: getIsTransition() }))
-  }
+    if (!shouldActivityUpdate(e)) return;
+    dispatch(
+      activity({
+        x: e.pageX,
+        y: e.pageY,
+        type: e.type,
+        isTransition: getIsTransition()
+      })
+    );
+  };
 
   const startActivityDetection = () => {
-    if(isBrowser()) activeEvents.forEach(x => document.addEventListener(x, onActivity))
-    dispatch(activityDetection(true))
-  }
+    if (isBrowser())
+      activeEvents.forEach(x => document.addEventListener(x, onActivity));
+    dispatch(activityDetection(true));
+  };
   const stopActivityDetection = () => {
-    if(isBrowser()) activeEvents.forEach(x => document.removeEventListener(x, onActivity))
-    dispatch(activityDetection(false))
-  }
-  return { startActivityDetection, stopActivityDetection }
-}
+    if (isBrowser())
+      activeEvents.forEach(x => document.removeEventListener(x, onActivity));
+    dispatch(activityDetection(false));
+  };
+  return { startActivityDetection, stopActivityDetection };
+};
 
+export const createDetection = ({
+  log,
+  activeEvents,
+  thresholds,
+  translateBlueprints
+}) => store => {
+  const { activity, activityDetection } = translateBlueprints({
+    activity: activityBlueprint,
+    activityDetection: activityDetectionBlueprint
+  });
 
-export const createDetection = ({ log, activeEvents, thresholds, translateBlueprints }) => store => {
-  const { activity
-        , activityDetection
-        } = translateBlueprints({ activity: activityBlueprint
-                                , activityDetection: activityDetectionBlueprint
-                                })
+  const getIsTransition = () => {
+    const idleStatus = store.getState().getIn([ROOT_STATE_KEY, "idleStatus"]);
 
+    return idleStatus !== IDLESTATUS_ACTIVE;
+  };
 
-  const getIsTransition = () => selectIdleState(store.getState()).idleStatus !== IDLESTATUS_ACTIVE
+  const {
+    startActivityDetection,
+    stopActivityDetection
+  } = createActivityDetection({
+    log,
+    thresholds,
+    activeEvents,
+    activity,
+    activityDetection,
+    getIsTransition
+  })(store);
+  const localSync = createLocalSync({ log, activity, getIsTransition })(store);
 
-  const { startActivityDetection, stopActivityDetection } = createActivityDetection({ log, thresholds, activeEvents, activity, activityDetection, getIsTransition })(store)
-  const localSync = createLocalSync({ log, activity, getIsTransition })(store)
+  invariant(
+    startActivityDetection,
+    "startActivityDetection should be a return property of createActivityDetection"
+  );
+  invariant(
+    stopActivityDetection,
+    "stopActivityDetection should be a return property of createActivityDetection"
+  );
+  invariant(localSync, "localSync should exist");
+  invariant(localSync.start, "localSync.start should exist");
+  invariant(localSync.stop, "localSync.stop should exist");
+  invariant(localSync.trigger, "localSync.trigger should exist");
 
-  invariant(startActivityDetection, 'startActivityDetection should be a return property of createActivityDetection')
-  invariant(stopActivityDetection, 'stopActivityDetection should be a return property of createActivityDetection')
-  invariant(localSync, 'localSync should exist')
-  invariant(localSync.start, 'localSync.start should exist')
-  invariant(localSync.stop, 'localSync.stop should exist')
-  invariant(localSync.trigger, 'localSync.trigger should exist')
+  log.info("activity detection starting");
 
-  log.info('activity detection starting')
-
-  return { startActivityDetection, stopActivityDetection, localSync }
-
-}
+  return { startActivityDetection, stopActivityDetection, localSync };
+};
--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -1,59 +1,94 @@
-import createContext from './context'
-import  { IS_DEV, IDLESTATUS_ACTIVE, START_BLUEPRINT, STOP_BLUEPRINT, GOTO_IDLE_STATUS_BLUEPRINT, ACTIVITY_BLUEPRINT, ACTIVITY_DETECTION_BLUEPRINT, NEXT_IDLE_STATUS_BLUEPRINT, LAST_IDLE_STATUS_BLUEPRINT } from './constants'
+import createContext from "./context";
+import {
+  IS_DEV,
+  IDLESTATUS_ACTIVE,
+  START_BLUEPRINT,
+  STOP_BLUEPRINT,
+  GOTO_IDLE_STATUS_BLUEPRINT,
+  ACTIVITY_BLUEPRINT,
+  ACTIVITY_DETECTION_BLUEPRINT,
+  NEXT_IDLE_STATUS_BLUEPRINT,
+  LAST_IDLE_STATUS_BLUEPRINT,
+  ROOT_STATE_KEY
+} from "./constants";
 
 /** When context has already been created, it can be shared to middleware component. */
 export const createReducer = context => {
-  const { initialState, translateBlueprintTypes, IDLE_STATUSES } = context
+  const { initialState, translateBlueprintTypes, IDLE_STATUSES } = context;
 
-  const IDLESTATUS_LAST = IDLE_STATUSES.slice(-1)[0]
+  const IDLESTATUS_LAST = IDLE_STATUSES.slice(-1)[0];
 
-  const { START
-        , STOP
-        , GOTO_IDLE_STATUS
-        , ACTIVITY
-        , ACTIVITY_DETECTION
-        , NEXT_IDLE_STATUS
-        , LAST_IDLE_STATUS
-        } = translateBlueprintTypes({ START: START_BLUEPRINT
-                                    , STOP: STOP_BLUEPRINT
-                                    , GOTO_IDLE_STATUS: GOTO_IDLE_STATUS_BLUEPRINT
-                                    , ACTIVITY: ACTIVITY_BLUEPRINT
-                                    , ACTIVITY_DETECTION: ACTIVITY_DETECTION_BLUEPRINT
-                                    , NEXT_IDLE_STATUS: NEXT_IDLE_STATUS_BLUEPRINT
-                                    , LAST_IDLE_STATUS: LAST_IDLE_STATUS_BLUEPRINT
-                                    })
-
+  const {
+    START,
+    STOP,
+    GOTO_IDLE_STATUS,
+    ACTIVITY,
+    ACTIVITY_DETECTION,
+    NEXT_IDLE_STATUS,
+    LAST_IDLE_STATUS
+  } = translateBlueprintTypes({
+    START: START_BLUEPRINT,
+    STOP: STOP_BLUEPRINT,
+    GOTO_IDLE_STATUS: GOTO_IDLE_STATUS_BLUEPRINT,
+    ACTIVITY: ACTIVITY_BLUEPRINT,
+    ACTIVITY_DETECTION: ACTIVITY_DETECTION_BLUEPRINT,
+    NEXT_IDLE_STATUS: NEXT_IDLE_STATUS_BLUEPRINT,
+    LAST_IDLE_STATUS: LAST_IDLE_STATUS_BLUEPRINT
+  });
 
   return (state = initialState, action = {}) => {
-    const { type, payload } = action
-    switch(type) {
+    const { type, payload } = action;
+    switch (type) {
       case START:
-        return Object.assign({}, state, selectStartPayload(payload))
+        return state.setIn([ROOT_STATE_KEY, "isRunning"], true);
       case STOP:
-        return Object.assign({}, state, selectStopPayload(payload))
-      case GOTO_IDLE_STATUS:
-        return Object.assign({}, state, selectGotoIdleStatusPayload(payload))
-      case ACTIVITY:
-        return Object.assign({}, state, selectActivityPayload(payload))
-      case ACTIVITY_DETECTION:
-        return Object.assign({}, state, selectActivityDetectionPayload(payload))
-      case NEXT_IDLE_STATUS:
-        return Object.assign({}, state, selectNextIdleStatusPayload(payload))
-      case LAST_IDLE_STATUS:
-        return Object.assign({}, state, selectLastIdleStatusPayload({ lastIdleStatus: IDLESTATUS_LAST }))
-      default:
+        return state.setIn([ROOT_STATE_KEY, "isRunning"], false);
+      case GOTO_IDLE_STATUS: {
+        const { idleStatus } = payload;
         return state
-    }
-  }
-}
+          .setIn([ROOT_STATE_KEY, "idleStatus"], idleStatus)
+          .setIn([ROOT_STATE_KEY, "isIdle"], true);
+      }
+      case ACTIVITY: {
+        const { activeStatus, lastActive, lastEvent, timeoutID } = payload;
 
-const selectStartPayload = () => ({ isRunning: true })
-const selectStopPayload = () => ({ isRunning: false })
-const selectGotoIdleStatusPayload = ({ idleStatus }) => ({ idleStatus, isIdle: true })
-const selectActivityPayload = ({ activeStatus, lastActive, lastEvent, timeoutID }) => ({ idleStatus: activeStatus, lastActive, lastEvent, timeoutID, isIdle: false })
-const selectActivityDetectionPayload = ({ isDetectionRunning }) => ({ isDetectionRunning })
-const selectNextIdleStatusPayload = ({ nextIdleStatus }) => ({ idleStatus: nextIdleStatus, isIdle: true })
-const selectLastIdleStatusPayload = ({ lastIdleStatus }) => ({ idleStatus: lastIdleStatus, isIdle: true })
+        return state
+          .setIn([ROOT_STATE_KEY, "idleStatus"], activeStatus)
+          .setIn([ROOT_STATE_KEY, "lastActive"], lastActive)
+          .setIn([ROOT_STATE_KEY, "lastEvent"], lastEvent)
+          .setIn([ROOT_STATE_KEY, "timeoutID"], timeoutID)
+          .setIn([ROOT_STATE_KEY, "isIdle"], false);
+      }
+
+      case ACTIVITY_DETECTION: {
+        const { isDetectionRunning } = payload;
+        return state.setIn(
+          [ROOT_STATE_KEY, "isDetectionRunning"],
+          isDetectionRunning
+        );
+      }
+      case NEXT_IDLE_STATUS: {
+        const { nextIdleStatus } = payload;
+        return state
+          .setIn([ROOT_STATE_KEY, "idleStatus"], nextIdleStatus)
+          .setIn([ROOT_STATE_KEY, "isIdle"], true);
+      }
+
+      case LAST_IDLE_STATUS: {
+        const lastIdleStatus = IDLESTATUS_LAST;
+
+        return state
+          .setIn([ROOT_STATE_KEY, "idleStatus"], lastIdleStatus)
+          .setIn([ROOT_STATE_KEY, "isIdle"], true);
+      }
+
+      default:
+        return state;
+    }
+  };
+};
 
 /** Creates reducer from opts including validation in development */
-export default function configureReducer (opts) { return createReducer(createContext(opts)) }
+export default function configureReducer(opts) {
+  return createReducer(createContext(opts));
+}
